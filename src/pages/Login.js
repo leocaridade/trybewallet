a788294b7936a38cf2fa766fca27c5b77cc642e4
leocaridade <leocaridade@hotmail.com>
2023-04-11 15:13:56 -0400

    Resolve o requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { userLogin } from '../redux/actions';
 
-class Login extends React.Component {
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+    history.push('/carteira');
+    dispatch(userLogin(email));
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const minPasswordLength = 6;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(email) && password.length >= minPasswordLength;
+
+    return (
+      <div>
+        <form onSubmit={ this.handleSubmit }>
+          <input
+            type="email"
+            data-testid="email-input"
+            name="email"
+            placeholder="Digite seu email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            placeholder="Digite sua senha"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+          <button
+            disabled={ !isValid }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+export const USER_LOGIN = 'USER_LOGIN';
+
+export const userLogin = (email) => ({
+  type: USER_LOGIN,
+  payload: {
+    email,
+  },
+});
